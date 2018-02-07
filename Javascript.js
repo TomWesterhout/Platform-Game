ad28a32225b617605217e966e3e5b2ef2ab418cb
TomWesterhout <westerhout_tom@hotmail.com>
2018-02-07 15:34:11 +0100

    Added a Level object method which checks whether a element overlaps with another element.

--- Javascript.js
+++ Javascript.js
@@ -169,8 +169,23 @@ DOMDisplay.prototype.clear = function() {
 var simpleLevel = new Level(simpleLevelPlan);
 var display = new DOMDisplay(document.body, simpleLevel);
 
+Level.prototype.obstacleAt = function(pos, size) {
+	var xStart = Math.floor(pos.x);
+	var xEnd = Math.ceil(pos.x + size.x);
+	var yStart = Math.floor(pos.y);
+	var yEnd = Math.ceil(pos.y + size.y);
 
-
+	if (xStart < 0 || xEnd > this.width || yStart < 0)
+		return "wall";
+	if (yEnd > this.height)
+		return "lava";
+	for (var y = yStart; y < yEnd; y++) {
+		for (var x = xStart; x < xEnd; x++) {
+			var fieldType = grid[y][x];
+			if (fieldType) return fieldType;
+		}
+	}
+};
 
 
 
