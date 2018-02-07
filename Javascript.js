0059503b7f73ed1176571e533c678d7666903057
TomWesterhout <westerhout_tom@hotmail.com>
2018-02-07 20:15:50 +0100

    Added act method to the Lava object which sets a new position for the object if no obstacle is found, otherwise it repeats its startposition or reverses its direction.

--- Javascript.js
+++ Javascript.js
@@ -214,6 +214,15 @@ Level.prototype.animate = function(step, keys) {
 	}
 };
 
+Lava.prototype.act = function(step, level) {
+	var newPos = this.pos.plus(this.speed.times(step));
+	if (!level.obstacleAt(newPos, this.size))
+		this.pos = newPos;
+	else if (this.repeatPos)
+		this.pos = this.repeatPos;
+	else
+		this.speed = this.speed.times(-1);
+};
 
 
 
