a04e950bd5cb76a18d4f492e5d909a9643dfd9d7
TomWesterhout <westerhout_tom@hotmail.com>
2018-02-13 00:46:54 +0100

    Changed minor spelling error.

--- Javascript.js
+++ Javascript.js
@@ -84,20 +84,20 @@ Level.prototype.animate = function(step, keys) {
 
 
 Level.prototype.playerTouched = function(type, actor) {
-	if (type == "lava" && this.status == null) {
-		this.status = "lost";
-		this.finishDelay = 1;
-	} else if (type == "coin") {
-		this.actors = this.actors.filter(function(other) {
-			return other != actor;
-		});
-		if (!this.actors.some(function(actor) {
-			return actor.type == "coin";
-		})) {
-			this.status = "won";
-			this.finishDelay = 1;
-		}
-	}
+  if (type == "lava" && this.status == null) {
+    this.status = "lost";
+    this.finishDelay = 1;
+  } else if (type == "coin") {
+    this.actors = this.actors.filter(function(other) {
+      return other != actor;
+    });
+    if (!this.actors.some(function(actor) {
+      return actor.type == "coin";
+    })) {
+      this.status = "won";
+      this.finishDelay = 1;
+    }
+  }
 };
 
 function Vector(x, y) {
@@ -129,49 +129,49 @@ Player.prototype.type = "player";
 var playerXSpeed = 7;
 
 Player.prototype.moveX = function(step, level, keys) {
-	this.speed.x = 0;
-	if (keys.left) this.speed.x -= playerXSpeed;
-	if (keys.right) this.speed.x += playerXSpeed;
+  this.speed.x = 0;
+  if (keys.left) this.speed.x -= playerXSpeed;
+  if (keys.right) this.speed.x += playerXSpeed;
 
-	var motion = new Vector(this.speed.x * step, 0);
-	var newPos = this.pos.plus(motion);
-	var obstacle = level.obstacleAt(newPos, this.size);
-	if (obstacle)
-		level.playerTouched(obstacle);
-	else
-		this.pos = newPos;
+  var motion = new Vector(this.speed.x * step, 0);
+  var newPos = this.pos.plus(motion);
+  var obstacle = level.obstacleAt(newPos, this.size);
+  if (obstacle)
+    level.playerTouched(obstacle);
+  else
+    this.pos = newPos;
 };
 
 var gravity = 30, jumpSpeed = 17;
 
 Player.prototype.moveY = function(step, level, keys) {
-	this.speed.y += gravity * step;
-	var motion = new Vector(0, this.speed.y * step);
-	var newPos = this.pos.plus(motion);
-	var obstacle = level.obstacleAt(newPos, this.size);
-	if (obstacle) {
-		level.playerTouched(obstacle);
-		if (keys.up && this.speed.y > 0)
-			this.speed.y -= jumpSpeed;
-		else
-			this.speed.y = 0;
-	} else {
-		this.pos = newPos;
-	}
+  this.speed.y += step * gravity;
+  var motion = new Vector(0, this.speed.y * step);
+  var newPos = this.pos.plus(motion);
+  var obstacle = level.obstacleAt(newPos, this.size);
+  if (obstacle) {
+    level.playerTouched(obstacle);
+    if (keys.up && this.speed.y > 0)
+      this.speed.y = -jumpSpeed;
+    else
+      this.speed.y = 0;
+  } else {
+    this.pos = newPos;
+  }
 };
 
 Player.prototype.act = function(step, level, keys) {
-	this.moveX(step, level, keys);
-	this.moveY(step, level, keys);
+  this.moveX(step, level, keys);
+  this.moveY(step, level, keys);
 
-	var otherActor = level.actorAt(this);
-	if (otherActor)
-		level.playerTouched(otherActor.type, otherActor);
+  var otherActor = level.actorAt(this);
+  if (otherActor)
+    level.playerTouched(otherActor.type, otherActor);
 
-	if (level.status == "lost") {
-		this.size.y -= step;
-		this.pos.y -= step;
-	}
+  if (level.status == "lost") {
+    this.pos.y += step;
+    this.size.y -= step;
+  }
 };
 
 function Lava(pos, ch) {
@@ -190,13 +190,13 @@ function Lava(pos, ch) {
 Lava.prototype.type = "lava";
 
 Lava.prototype.act = function(step, level) {
-	var newPos = this.pos.plus(this.speed.times(step));
-	if (!level.obstacleAt(newPos, this.size))
-		this.pos = newPos;
-	else if (this.repeatPos)
-		this.pos = this.repeatPos;
-	else
-		this.speed = this.speed.times(-1);
+  var newPos = this.pos.plus(this.speed.times(step));
+  if (!level.obstacleAt(newPos, this.size))
+    this.pos = newPos;
+  else if (this.repeatPos)
+    this.pos = this.repeatPos;
+  else
+    this.speed = this.speed.times(-1);
 };
 
 function Coin(pos) {
